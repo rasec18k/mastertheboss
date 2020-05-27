7b167012fe71772fcc34ef3d9cd01b704e5e362f
Cesar Rios Torres <cesar.rios@mdp.com.pe>
2020-05-27 02:34:35 -0500

    hostname

--- ejb/remote-ejb-latest/ejb-client-basic/src/main/java/com/itbuzzpress/chapter4/client/RemoteEJBClient.java
+++ ejb/remote-ejb-latest/ejb-client-basic/src/main/java/com/itbuzzpress/chapter4/client/RemoteEJBClient.java
@@ -45,7 +45,7 @@ public class RemoteEJBClient {
         final Hashtable<String, String> jndiProperties = new Hashtable<>();
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
             //use HTTP upgrade, an initial upgrade requests is sent to upgrade to the remoting protocol
-            jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
+            jndiProperties.put(Context.PROVIDER_URL,"remote+http://eap-app-rasec.sandbox-dev.svc.cluster.local:8080");
       final Context context = new InitialContext(jndiProperties);
 
 /*
@@ -53,9 +53,9 @@ public class RemoteEJBClient {
 		final Hashtable jndiProperties = new Hashtable();
  
 		jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY,  "org.wildfly.naming.client.WildFlyInitialContextFactory");
-                jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
+                jndiProperties.put(Context.PROVIDER_URL,"remote+http://eap-app-rasec.sandbox-dev.svc.cluster.local:8080");
                 // Pure HTTP
-		//jndiProperties.put(Context.PROVIDER_URL,"http://localhost:8080/wildfly-services");
+		//jndiProperties.put(Context.PROVIDER_URL,"http://eap-app-rasec.sandbox-dev.svc.cluster.local:8080/wildfly-services");
 
 		final Context ctx = new InitialContext(jndiProperties);
  
@@ -69,7 +69,7 @@ public class RemoteEJBClient {
 	      final Hashtable<String, String> jndiProperties = new Hashtable<>();
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
             //use HTTP upgrade, an initial upgrade requests is sent to upgrade to the remoting protocol
-            jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
+            jndiProperties.put(Context.PROVIDER_URL,"remote+http://eap-app-rasec.sandbox-dev.svc.cluster.local:8080");
       final Context context = new InitialContext(jndiProperties);
 
 		return (Calculator) context
